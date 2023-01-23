5267765348142215ac66af3c587fee05a7b17149
prajaktadharpure28 <prajaktadharpure28@gmail.com>
2023-01-23 23:59:42 +0530

    feat: added create food item api route

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import User from './models/User.js';
+import FoodItem from './models/FoodItem.js';
 
 const app = express();
 app.use(express.json());
@@ -100,6 +101,24 @@ app.post('/login', async(req, res)=>{
             message: "Invalid email or password"
         })
     }
+})
+
+app.post("/createFoodItem", async(req, res)=>{
+    const {title, description, imgUrl, price, category} = req.body;
+    const foodItem = new FoodItem({
+        title: title,
+        description: description,
+        imgUrl: imgUrl,
+        price: price,
+        category: category
+    })
+
+    const savedFoodItem = await foodItem.save();
+    res.json({
+        success: true,
+        message: "Food Item created successfully",
+        data: savedFoodItem
+    })
 })
 
 // api routes ends here
