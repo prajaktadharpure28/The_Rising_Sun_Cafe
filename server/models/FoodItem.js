20638c8c0c32960e4df209dc2570bbf445f09315
prajaktadharpure28 <prajaktadharpure28@gmail.com>
2023-07-02 17:31:33 +0530

    update food item model

---
+++ server/models/FoodItem.js
@@ -0,0 +1,13 @@
+
+const mongoose=require("mongoose")
+const {Schema,model} =mongoose
+const foodItemSchema = new Schema({
+    title: String,
+    description: String,
+    imgUrl: String,
+    price: Number,
+    category: String
+});
+
+const FoodItem = model("FoodItem", foodItemSchema);
+module.exports=FoodItem
